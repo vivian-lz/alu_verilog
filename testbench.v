584e46da2def2f0c2cf569886ca19cfd2c573e51
Vivian Zeru <vivian.zeru@gmail.com>
2025-07-10 18:53:51 -0400

    fix syntax

--- testbench.v
+++ testbench.v
@@ -12,157 +12,159 @@ module testbench;
   alu_4bit dut4 (a4, b4, opcode, cin4, result4, cout4);
   initial begin
     //ADD FUNCTION
-    a1 = 1; b1 = 0; opcode = 2'b00; cin1 = 0; //Add No Carry
+    a1 = 1'b1; b1 = 1'b0; opcode = 2'b00; cin1 = 1'b0; //Add No Carry
     #10
-    if(result1 !== 1 || cout1 !== 0)
+    if(result1 !== 1'b1 || cout1 !== 1'b0)
       $display("Test failed for 1 bit: Correct answer is 1 with 0 carry");
     else
       $display("Test passed for 1 bit.");
     
-    a4 = 1001; b4 = 0110; opcode = 2'b00; cin4 = 0; //Add No Carry
+    a4 = 4'b1001; b4 = 4'b0110; opcode = 2'b00; cin4 = 1'b0; //Add No Carry
     #10
-    if(result4 != 1111 || cout4 != 0)
+    if(result4 !== 4'b1111 || cout4 !== 1'b0)
       $display("Test failed for 4 bit: Correct answer is 1111 with 0 carry");
     else
       $display("Test passed for 4 bit.");
     
-    a1 = 1; b1 = 1; opcode = 2'b00; cin1 = 0; //Add Carry
+    a1 = 1'b1; b1 = 1'b1; opcode = 2'b00; cin1 = 1'b0; //Add Carry
     #10
-    if(result1 !== 0 || cout1 !== 1)
+    if(result1 !== 1'b0 || cout1 !== 1'b1)
       $display("Test failed for 1 bit: Correct answer is 0 with 1 carry");
     else
       $display("Test passed for 1 bit.");
     
-    a4 = 1001; b4 = 0111; opcode = 2'b00; cin = 0; //Add carry
+    a4 = 4'b1001; b4 = 4'b0111; opcode = 2'b00; cin4 = 1'b0; //Add carry
     #10
-    if(result4 !== 0000 || cout !== 1)
+    if(result4 !== 4'b0000 || cout4 !== 1'b1)
       $display("Test failed for 4 bit: Correct answer is 0000 with 1 carry.");
+    else
+      $display("Test passed for 4 bit.");
     
     //SUBTRACT FUNCTION
-    a1 = 1; b1 = 0; opcode = 2'b01; cin1 = 0; //Subtract
+    a1 = 1'b1; b1 = 1'b0; opcode = 2'b01; cin1 = 1'b0; //Subtract
     #10
-    if(result1 !== 1 || cout1 !== 0)
+    if(result1 !== 1'b1 || cout1 !== 1'b0)
       $display("Test failed for 1 bit: Correct answer is 1 with 0 carry");
     else
       $display("Test passed for 1 bit.");
     
-    a4 = 1111; b4 = 0000; opcode = 2'b01; cin = 0;
+    a4 = 4'b1111; b4 = 4'b0000; opcode = 2'b01; cin4 = 1'b0;
     #10
-    if(result4 !== 1111 || cout !== 0)
+    if(result4 !== 4'b1111 || cout4 !== 1'b0)
       $display("Test failed for 4 bit: correct answer is 1111 with 0 carry.");
     else
       $display("Test passed for 4 bit.");
     
     //AND FUNCTION
-    a1 = 1; b1 = 1; opcode = 2'b10; cin1 = 0; //AND 11
+    a1 = 1'b1; b1 = 1'b1; opcode = 2'b10; cin1 = 1'b0; //AND 11
     #10
-    if(result1 !== 1)
+    if(result1 !== 1'b1)
       $display("Test failed for 1 bit: Correct answer is 1");
     else
       $display("Test passed for 1 bit.");
     
-    a1 = 1; b1 = 0; opcode = 2'b10; cin1 = 0; //AND 10
+    a1 = 1'b1; b1 = 1'b0; opcode = 2'b10; cin1 = 1'b0; //AND 10
     #10
-    if(result1 !== 0)
+    if(result1 !== 1'b0)
       $display("Test failed for 1 bit: Correct answer is 0");
     else
       $display("Test passed for 1 bit.");
     
-    a1 = 0; b1 = 1; opcode = 2'b10; cin1 = 0; //AND 01
+    a1 = 1'b0; b1 = 1'b1; opcode = 2'b10; cin1 = 1'b0; //AND 01
     #10
-    if(result1 !== 0)      
+    if(result1 !== 1'b0)      
       $display("Test failed for 1 bit: Correct answer is 0");
     else
       $display("Test passed for 1 bit.");
     
-    a1 = 0; b = 0; opcode = 2'b10; cin1 = 0; //AND 00
+    a1 = 1'b0; b1 = 1'b0; opcode = 2'b10; cin1 = 1'b0; //AND 00
     #10
-    if(result1 !== 0)
+    if(result1 !== 1'b0)
       $display("Test failed for 1 bit: Correct answer is 0");
     else
       $display("Test passed for 1 bit.");
     
-    a4 = 0000; b4 = 0000; opcode = 2'b10; cin = 0;
+    a4 = 4'b0000; b4 = 4'b0000; opcode = 2'b10; cin4 = 1'b0;
     #10
-    if(result4 != 0000)
+    if(result4 !== 4'b0000)
       $display("Test failed for 4 bit: Correct answer is 0000.");
     else
       $display("Test passed for 4 bit.");
     
-    a4 = 1010; b4 = 0101; opcode = 2'b10; cin = 0;
+    a4 = 4'b1010; b4 = 4'b0101; opcode = 2'b10; cin4 = 1'b0;
     #10
-    if(result4 !== 0000)
+    if(result4 !== 4'b0000)
       $display("Test failed for 4 bit: Correct answer is 0000.");
     else
       $display("Test passed for 4 bit.");
     
-    a4 = 1010; b4 = 1111; opcode = 2'b10; cin = 0;
+    a4 = 4'b1010; b4 = 4'b1111; opcode = 2'b10; cin4 = 1'b0;
     #10
-    if(result4 !== 1010)
+    if(result4 !== 4'b1010)
       $display("Test failed for 4 bit: Correct answer is 1010.");
     else
       $display("Test passed for 4 bit.");
     
-    a4 = 1111; b4 = 1111; opcode = 2'b10; cin = 0;
+    a4 = 4'b1111; b4 = 4'b1111; opcode = 2'b10; cin4 = 1'b0;
     #10
-    if(result4 !== 1111)
+    if(result4 !== 4'b1111)
       $display("Test failed for 4 bit: Correct answer is 1111.");
     else
       $display("Test passed for 4 bit.");
     
     //OR FUNCTION
-    a1 = 1; b1 = 1; opcode = 2'b11; cin1 = 0; //OR 11
+    a1 = 1'b1; b1 = 1'b1; opcode = 2'b11; cin1 = 1'b0; //OR 11
     #10
-    if(result1 !== 1)
+    if(result1 !== 1'b1)
       $display("Test failed for 1 bit: Correct answer is 1");
     else
       $display("Test passed for 1 bit.");
     
-    a1 = 1; b1 = 0; opcode = 2'b11; cin1 = 0; //OR 10
+    a1 = 1'b1; b1 = 1'b0; opcode = 2'b11; cin1 = 1'b0; //OR 10
     #10
-    if(result1 !== 0)
+    if(result1 !== 1'b1)
       $display("Test failed for 1 bit: Correct answer is 1");
     else
       $display("Test passed for 1 bit.");
     
-    a1 = 0; b1 = 1; opcode = 2'b11; cin1 = 0; //OR 01
+    a1 = 1'b0; b1 = 1'b1; opcode = 2'b11; cin1 = 1'b0; //OR 01
     #10
-    if(result1 !== 1)      
+    if(result1 !== 1'b1)      
       $display("Test failed for 1 bit: Correct answer is 1");
     else
       $display("Test passed for 1 bit.");
     
-    a1 = 0; b1 = 0; opcode = 2'b11; cin1 = 0; //OR 00
+    a1 = 1'b0; b1 = 1'b0; opcode = 2'b11; cin1 = 1'b0; //OR 00
     #10
-    if(result1 !== )0
+    if(result1 !== 1'b0)
       $display("Test failed for 1 bit: Correct answer is 0");
     else
       $display("Test passed for 1 bit.");
     
-    a4 = 0000; b4 = 0000; opcode = 2'b11; cin = 0;
+    a4 = 4'b0000; b4 = 4'b0000; opcode = 2'b11; cin4 = 1'b0;
     #10
-    if(result4 != 0000)
+    if(result4 !== 4'b0000)
       $display("Test failed for 4 bit: Correct answer is 0000.");
     else
       $display("Test passed for 4 bit.");
     
-    a4 = 1010; b4 = 0101; opcode = 2'b11; cin = 0;
+    a4 = 4'b1010; b4 = 4'b0101; opcode = 2'b11; cin4 = 1'b0;
     #10
-    if(result4 !== 1111)
+    if(result4 !== 4'b1111)
       $display("Test failed for 4 bit: Correct answer is 1111.");
     else
       $display("Test passed for 4 bit.");
     
-    a4 = 1010; b4 = 0000; opcode = 2'b11; cin = 0;
+    a4 = 4'b1010; b4 = 4'b0000; opcode = 2'b11; cin4 = 1'b0;
     #10
-    if(result4 !== 1010)
+    if(result4 !== 4'b1010)
       $display("Test failed for 4 bit: Correct answer is 1010.");
     else
       $display("Test passed for 4 bit.");
     
-    a4 = 1111; b4 = 1111; opcode = 2'b11; cin = 0;
+    a4 = 4'b1111; b4 = 4'b1111; opcode = 2'b11; cin4 = 1'b0;
     #10
-    if(result4 !== 1111)
+    if(result4 !== 4'b1111)
       $display("Test failed for 4 bit: Correct answer is 1111.");
     else
       $display("Test passed for 4 bit.");
