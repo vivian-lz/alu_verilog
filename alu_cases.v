5da61cea9b66972729ab9d27d27dfeceeaf447fe
Vivian Zeru <vivian.zeru@gmail.com>
2025-07-10 18:54:14 -0400

    fix syntax

--- alu_cases.v
+++ alu_cases.v
@@ -1,7 +1,7 @@
 //This utilizes cases as the base of the ALU
 
 //1 bit alu using a 2 bit opcode
-module alu_1bit(input a, input b, input [1:0] opcode, input cin, output result, output cout);
+module alu_1bit(input a, input b, input [1:0] opcode, input cin, output reg result, output reg cout);
   always @(*) begin
     case (opcode)
       2'b00: {result, cout} = a + b + cin; //addition
@@ -20,12 +20,12 @@ endmodule
 module alu_4bit(
   input[3:0] a, b, 
   input [1:0] opcode, 
-  input [3:0] result, 
-  output cout);
+  input cin,
+  output [3:0] reg result, 
+  output reg cout);
   wire [2:0] carry_internal;
   alu_1bit bit0 (a[0], b[0], opcode, cin, result[0], carry_internal[0]);
   alu_1bit bit1 (a[1], b[1], opcode, carry_internal[0], result[1], carry_internal[1]);
   alu_1bit bit2 (a[2], b[2], opcode, carry_internal[1], result[2]. carry_internal[2]);
   alu_1bit bit3(a[3], n[3], opcode, carry_internal[2], result[3], cout);
 endmodule
-
