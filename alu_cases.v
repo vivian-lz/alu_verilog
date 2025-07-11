b3498141b1a65e66548e12473a6222dc81b0ff61
Vivian Zeru <vivian.zeru@gmail.com>
2025-07-10 20:09:46 -0400

    more syntax

--- alu_cases.v
+++ alu_cases.v
@@ -15,17 +15,18 @@ module alu_1bit(input a, input b, input [1:0] opcode, input cin, output reg resu
         cout = 1'b0;
       end
     endcase
+  end
 endmodule
 
 module alu_4bit(
   input[3:0] a, b, 
   input [1:0] opcode, 
   input cin,
-  output [3:0] reg result, 
-  output reg cout);
+  output [3:0] result, 
+  output cout);
   wire [2:0] carry_internal;
   alu_1bit bit0 (a[0], b[0], opcode, cin, result[0], carry_internal[0]);
   alu_1bit bit1 (a[1], b[1], opcode, carry_internal[0], result[1], carry_internal[1]);
-  alu_1bit bit2 (a[2], b[2], opcode, carry_internal[1], result[2]. carry_internal[2]);
-  alu_1bit bit3(a[3], n[3], opcode, carry_internal[2], result[3], cout);
+  alu_1bit bit2 (a[2], b[2], opcode, carry_internal[1], result[2], carry_internal[2]);
+  alu_1bit bit3(a[3], b[3], opcode, carry_internal[2], result[3], cout);
 endmodule
