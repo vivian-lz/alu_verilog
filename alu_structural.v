2159cfcb1edbb869d8482851ef069e3f7ab42ca6
Vivian Zeru <vivian.zeru@gmail.com>
2025-07-11 21:52:20 -0400

    fixed structural logic

--- alu_structural.v
+++ alu_structural.v
@@ -1,11 +1,3 @@
-module mux_2to1( 
-    	input a, b, sel,
-    	output wire out ); 
-    
-  assign out = (sel == 1'b0) ? a : b;
-
-endmodule
-    
 module full_adder(input a, input b, input cin, output wire sum, output wire cout);
 	wire middle, and1, and2;
   
@@ -16,31 +8,23 @@ module full_adder(input a, input b, input cin, output wire sum, output wire cout
   	assign cout = and1 | and2;
   
 endmodule
-
-module logical_and(input a, input b, output wire result);
-  assign result = a & b;
-endmodule
-
-module logical_or(input a, input b, output wire result);
-  assign result = a | b;
-endmodule
     
-module alu_1bit(input a, input b, input [1:0] opcode, input cin, output result, output cout);
-  wire mux_wire;
-  	//full adder and mux
-  always @(*) begin
-  	if(opcode == 2'b00)
-    	mux_2to1 mux1 (b, ~b, 1'b0, mux_wire);
-  		full_adder adder1 (a, mux_wire, cin, result, cout);
- 	else if(opcode == 2'b01)
-    	mux_2to1 mux2 (b, ~b, 1'b1, mux_wire);
-  		full_adder adder2 (a, mux_wire, cin, result, cout);
-  	else if(opcode == 2'b10)
-  		logical_and andcase (a, b, result);
-  	else //opcode 11
-  		logical_or orcase (a, b, result);
-  	end
-  end
+module alu_1bit(input a, input b, input [1:0] opcode, input cin, output wire result, output wire cout);
+  wire mux_wire, sum, cout_internal, and_internal, or_internal, cin_internal;
+  //full adder and mux
+  assign mux_wire = (opcode == 2'b00) ? b : ~b; //acts as an mux select
+  assign cin_internal = (opcode == 2'b01) ? 1'b1 : cin;
+  
+  full_adder adder (a, mux_wire, cin_internal, sum, cout_internal);
+  assign and_internal = a & b;
+  assign or_internal = a | b;
+  
+  assign result = (opcode[1] == 0) ? sum : 
+    (opcode == 2'b10) ? and_internal :
+    or_internal;
+  
+  assign cout = (opcode[1] == 0) ? cout_internal : 1'b0;
+  
 endmodule
 
 module alu_4bit(input [3:0] a, input [3:0] b, input [1:0] opcode, input cin, output [3:0] result, output cout);
@@ -50,4 +34,3 @@ module alu_4bit(input [3:0] a, input [3:0] b, input [1:0] opcode, input cin, out
   alu_1bit bit2 (a[2], b[2], opcode, carry_internal[1], result[2], carry_internal[2]);
   alu_1bit bit3(a[3], b[3], opcode, carry_internal[2], result[3], cout);
 endmodule
-
